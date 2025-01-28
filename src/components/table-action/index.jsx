1c37209316ccf6fcdb77b1a5398f0781c293f1c5
Jin Tolentino <tolentino.jin@gmail.com>
2025-01-28 18:50:35 +0800

    update

--- src/components/table-action/index.jsx
+++ src/components/table-action/index.jsx
@@ -23,11 +23,31 @@ function TableAction({email, password}) {
   }
 }
 
+const restoreButton = async () => {
+  try {
+    const response = await fetch('http://localhost:5000/api/updateUser', {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+        },
+        body: JSON.stringify({ email:email,status:"Active",password : password }),
+    });
+    console.log(response);
+    if(response.ok){
+      alert("User Restored")
+    }
+ }catch(error) {
+  console.error('Error restoring users:', error);
+  alert('An occured during restoring users:'+ error);
+}
+}
+
 
   return (
     <div id="action-container">
       <button id="edit-btn">Edit</button>
       <button id="delete-btn" onClick={deleteButton}>Delete</button>
+      <button id="restore-btn" onClick={restoreButton}>Restore</button>
     </div>
   )
 }
